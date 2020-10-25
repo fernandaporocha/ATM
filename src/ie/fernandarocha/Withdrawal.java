ea1cc20b0f5fc7217d49060677b5d24aa7616b7c
Fernanda Rocha <fernandaporocha@gmail.com>
2020-10-25 18:22:37 +0000

    Adding references to associated classes

--- src/ie/fernandarocha/Withdrawal.java
+++ src/ie/fernandarocha/Withdrawal.java
@@ -5,6 +5,11 @@ public class Withdrawal {
 	private int accountNumber;
 	private double amount;
 	
+	private Screen screen;
+	private Keypad keypad;
+	private CashDispenser cashDispenser;
+	private BankDatabase bankDatabase;
+	
 	public Withdrawal() {}
 	
 	public void execute() {}
