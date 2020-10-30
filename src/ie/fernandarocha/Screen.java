55f54d7322baf49814270dd4c6c907d8b6d1239f
Fernanda Rocha <fernandaporocha@gmail.com>
2020-10-30 14:43:45 +0000

    Implementing the methods

--- src/ie/fernandarocha/Screen.java
+++ src/ie/fernandarocha/Screen.java
@@ -4,6 +4,16 @@ public class Screen {
 	
 	public Screen() {}
 	
-	public void displayMessage() {}
+	public void displayMessageLine(String message) {
+		System.out.println(message);
+	}
+	
+	public void displayMessage(String message) {
+		System.out.print(message);
+	}
+	
+	public void displayEuroAmount(double amount) {
+		System.out.printf("€%,.2f", amount);
+	}
 
 }
